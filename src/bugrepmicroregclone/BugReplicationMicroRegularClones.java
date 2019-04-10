32192bbca7b0566563fbaa9c8aa478d6918bf605
Judith <Judith@10.81.6.55>
2019-04-09 18:31:34 -0600

    Initializing BugReplicationMicroRegularClones.java.

---
+++ src/bugrepmicroregclone/BugReplicationMicroRegularClones.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bugrepmicroregclone;
+
+/**
+ *
+ * @author Judith
+ */
+public class BugReplicationMicroRegularClones {
+    
+    public void bugReplication(){
+    
+    }
+    
+}
